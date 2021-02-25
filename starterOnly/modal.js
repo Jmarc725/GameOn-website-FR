c9a6082d6207989ec3a5e0064657b1fef1977c2c
Jean <jrc.lemaire@gmail.com>
2021-02-25 15:45:16 -0800

    issue 1 fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,9 +1,9 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var myTopNav = document.getElementById("myTopnav");
+  if (myTopNav.className === "topnav") {
+    myTopNav.className += " responsive";
   } else {
-    x.className = "topnav";
+    myTopNav.className = "topnav";
   }
 }
 
@@ -11,13 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector('.close');
 
 // launch modal event
+// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+close.addEventListener('click', () => {
+  modalbg.style.display = "none";
+});
 
 
