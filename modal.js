05b27fe7a50dea28836f233dddfad1c5b14f25b9
Jean <jrc.lemaire@gmail.com>
2021-04-12 18:32:11 -0700

    projet terminé

--- modal.js
+++ modal.js
@@ -1,38 +1,12 @@
-function editNav() {
-  const myTopNav = document.getElementById("myTopnav")
-  if (myTopNav.className === "topnav") {
-    myTopNav.className += " responsive"
-  } else {
-    myTopNav.className = "topnav"
-  }
-}
+// ---------------------------------------------------- Variables
 
-// DOM Elements
+
 const modalbg = document.querySelector(".bground")
 const modalBtn = document.querySelectorAll(".modal-btn")
 const formData = document.querySelectorAll(".formData")
-const close = document.querySelector('.close') // Je crée une variable pour sélectionner le span qui fermea la modal
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+const close = document.querySelector('.close') 
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block"   // J'affiche  le formulaire
-  reservationForm.style.display = "block"   // Je réactive le formulaire après sa fermeture dès que je relance la modal
-  successMessage.style.display = "none"   // Je masque le message de validation
-}
-
-// J'écoute l'événement click pour fermer la modal
-close.addEventListener('click',function() {
-  modalbg.style.display = "none"
-});
-
-
-//-------------------------------------------------------
-
-
-// Je déclare mes variables
 const firstNameInput = document.querySelector('#first')
 const firstNameErrorMessage = document.querySelector('.first-name-error-message')
 
@@ -42,11 +16,11 @@ const lastNameErrorMessage = document.querySelector('.last-name-error-message')
 const emailInput = document.querySelector('#email')
 const emailInputErrorMessage = document.querySelector('.email-error-message')
 
-const tournamentNumberInput = document.querySelector('#quantity')
-const tournamentNumberErrorMessage = document.querySelector('.number-error-message')
-
 const calendarInput = document.querySelector('#birthdate')
 const calendarErrorMessage = document.querySelector('.error-message-birthdate')
+
+const tournamentNumberInput = document.querySelector('#quantity')
+const tournamentNumberErrorMessage = document.querySelector('.number-error-message')
 
 const citiesLocation = document.querySelectorAll('.citiesLocation input')
 const citiesLocationErrorMessage = document.querySelector('.citiesLocation-error-message')
@@ -59,10 +33,26 @@ const successMessage = document.querySelector('.success-message')
 const closeButton = document.querySelector('.btn-close')
 
 
-// ---------------------------------------------------------------------------------
+// ----------------------------------------------------- Fonctions
 
 
-// Je déclre mes fonctions
+// Navigation responsive
+function editNav() {
+  const myTopNav = document.getElementById("myTopnav")
+  if (myTopNav.className === "topnav") {
+    myTopNav.className += " responsive"
+  } else {
+    myTopNav.className = "topnav"
+  }
+}
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block"   // J'affiche  le formulaire
+  reservationForm.style.display = "block"   // Je réactive le formulaire après sa fermeture dès que je relance la modal
+  successMessage.style.display = "none"   // Je masque le message de validation
+}
+
 // Fonction validation prénom
 function checkFirstName() {
   const isFirstNameValid = firstNameInput.value.length > 2
@@ -102,7 +92,7 @@ function checkEmailInput() {
 
 // Fonction validation date de calendrier
 function checkCalendarInput(){
-  const isCalendarValid = calendarInput.value
+  const isCalendarValid = calendarInput.value.length > 0
 
   if (isCalendarValid){
     calendarErrorMessage.classList.add('hidden')
@@ -148,22 +138,48 @@ function checkconditionsInput(){
     requiredCheckboxErrorMessage.classList.add('hidden')
   } else {
     requiredCheckboxErrorMessage.classList.remove('hidden')
-}
-return isConditionsValid
+  }
+  return isConditionsValid
 }
 
-// ------------------------------------------------------------------------------
+// Reset form
+function cleanForm() {
+  firstNameInput.value = ""
+  lastNameInput.value = ""
+  emailInput.value = ""
+  calendarInput.value = ""
+  tournamentNumberInput.value = ""
+  for (let i = 0; i < citiesLocation.length; i++) {
+    citiesLocation[i].checked = false
+  }
+  requiredCheckbox.checked = false
+}
+
+// ---------------------------------------------- Event Listener
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+
+
+// J'écoute l'événement click pour fermer la modal
+close.addEventListener('click',function() {
+  modalbg.style.display = "none"
+})
 
 // J'écoute l'évenement submit pour valider mon formulaire
 reservationForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  if (checkFirstName() && checkLastName() && checkEmailInput() && checkCityLocation() 
-  && checkTournamentNumberInput() && checkconditionsInput() && checkCalendarInput()){
+  if (checkFirstName() && checkLastName() && checkEmailInput() && checkCalendarInput()  
+  && checkTournamentNumberInput() && checkCityLocation() && checkconditionsInput()){
     console.log("Tout est ok") // Je vérifie dans ma console
     reservationForm.style.display = "none"  // Je masque le formulaire
     successMessage.style.display = "flex"  // J'affiche le message de validation
-
+      
+    // reset
+    cleanForm()
+     
   } else {
     console.log("Il y a un problème") // Je vérifie dans ma console
   }
@@ -171,7 +187,8 @@ reservationForm.addEventListener('submit', function(event) {
 
 // J'écoute l'évenement click pour fermer la modal
 closeButton.addEventListener('click', function() {
-modalbg.style.display = 'none'
+  modalbg.style.display = 'none'
 })
+
 
 
